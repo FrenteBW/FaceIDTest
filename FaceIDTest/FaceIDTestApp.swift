50865735768b4ee3545efa172498d125369f8f05
An Byungwook <abw2619@icloud.com>
2023-10-26 16:01:40 +0900

    Initial Commit

---
+++ FaceIDTest/FaceIDTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIDTestApp.swift
+//  FaceIDTest
+//
+//  Created by 안병욱의 mac on 2023/10/26.
+//
+
+import SwiftUI
+
+@main
+struct FaceIDTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
