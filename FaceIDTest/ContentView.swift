ba72f2e31ef2035e00cc87da335183b3e8ec225a
An Byungwook <abw2619@icloud.com>
2023-10-26 16:22:02 +0900

    First commit

--- FaceIDTest/ContentView.swift
+++ FaceIDTest/ContentView.swift
@@ -6,19 +6,55 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+    
+    @State private var isUnlocked = false
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data"
+
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    DispatchQueue.main.async {
+                        isUnlocked = true
+                    }
+                } else {
+                    // there was a problem
+                    DispatchQueue.main.async {
+                        isUnlocked = false
+                    }
+                }
+            }
+        } else {
+            // no biometrics
+            isUnlocked = false
+        }
+    }
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if isUnlocked {
+                Text("Unlocked screen")
+            } else {
+                Text("Locked screen")
+            }
+            
+            Button(action: {
+                authenticate()
+            }) {
+                Text("Authenticate with Face ID")
+            }
         }
         .padding()
     }
-}
-
-#Preview {
-    ContentView()
 }
